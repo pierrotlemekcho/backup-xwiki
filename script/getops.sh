0bbd48d4137dc507c1ca6bf817562051b71c3cc0
pierre BAIZEAU <pierre@sif-revetement.com>
2020-02-28 17:58:52 +0100

    essai passage argument

--- script/getops.sh
+++ script/getops.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 
 # script de sauvegarde des fichiers Apache nécessaire à une restauration complète sur UBUNTU SERVER 18.04
 # pour le fonctionnement de xwiki apache tomcat avec mod -jk
@@ -10,7 +9,7 @@
 
 
 usage() { echo "
-# usage bactar [-j] [-s] [-m]
+# usage bactar [-j] [-s] [-m] [-u username]
 # -j sauvegarde journaliere --> le repertoire lundi/mardi/mercredi....../dimanche
 # -s sauvegarde hebdomadaire --> le repertoire 1 2 ...52
 # -m sauvegarde manuelle --> le repertoire=$(date '+%Y-%m-%d-%H-%M-%S')
@@ -22,23 +21,24 @@ usage() { echo "
 # choix du de la date de sauvegarde
 ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-while getopts ":jsm:u" opt
+while getopts ":jsmu:" opt
 do
-	echo " option choisi $opt
+	echo " option choisi $opt"
 	case $opt in
 
 		j) DATE=$(date '+%A');;
 		s) DATE=$(date '+%W');;
 		m) DATE=$(date '+%Y-%m-%d-%H-%M');;
 		u) echo " le user est : $OPTARG"
-			user=$OPTARG
-		\?) usage; exit;; 
+			user=$OPTARG;;
+		\?) usage
+	       		exit 1 ;; 
 
 	esac
 done
 
 
-echo " sauvegarde dans dans le repertoire :$DATE"
+echo " sauvegarde dans dans le repertoire :$DATE de l opératuer : $user "
 
 
 ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -46,4 +46,4 @@ echo " sauvegarde dans dans le repertoire :$DATE"
 ###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 source /home/$user/backup/script/repertoires.cnf
-echo " variable de repertoires.cnf pour verifier"
+echo " variable de repertoires.cnf SAUVEFOLD : $SAUVEFOLDERX"
